3cf4b66ed2325cf8209606287f2bc7b710db83bf
codemande <1o0xday.2a10@gmail.com>
2025-09-23 04:59:28 +0100

    Add mobile view and style

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,14 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { FaFacebook, FaInstagram, FaYoutube, FaShoppingCart } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdClose } from "react-icons/md";
 import './styles/Header.css';
 
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <header>
       <div className="header-desktop-container container">
@@ -13,7 +18,7 @@ function Header() {
           <h1> EAGLE STORE </h1>
         </Link>
        
-        <div className="header-desktop-links">
+        <div className="header-desktop-links desktop-only">
 
           <ul className="desktop-pages-links links">
             <li><Link>Home</Link></li>
@@ -39,11 +44,44 @@ function Header() {
 
           <Link className="cart-icon-link">
             <FaShoppingCart className="cart-icon"  />
-            <span className="cart-count">0</span>
+            <span className="cart-count bounce">0</span>
           </Link>
-          
         </div>
+
+        {/* Mobile menu toggle */}
+        <button
+          className="mobile-only"
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          { menuOpen === false ? <GiHamburgerMenu /> : <MdClose />}
+          
+        </button>
       </div>
+
+      {/* Mobile Nav Panel */}
+        {menuOpen && (
+          <div className="mobile-nav">
+            <Link onClick={() => setMenuOpen(false)}>
+              Home
+            </Link>
+            <Link onClick={() => setMenuOpen(false)}>
+              Shop
+            </Link>
+            <Link onClick={() => setMenuOpen(false)}>
+              About
+            </Link>
+            <Link onClick={() => setMenuOpen(false)}>
+              Contact
+            </Link>
+            <Link onClick={() => setMenuOpen(false)}    className="cart-link-mobile">
+              <span style={{ position: "relative" }}>
+                <FaShoppingCart className="cart-icon"   />
+                <span  className="cart-count">0</span>
+              </span>
+              Cart
+            </Link>
+          </div>
+        )}
     </header>
   )
 }
