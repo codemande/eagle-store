a035ed3a08a6ace83b419786fdfd4b4800e74e03
codemande <1o0xday.2a10@gmail.com>
2025-09-26 10:09:48 +0100

    Input Shop header and title, make responsive

--- client/src/pages/Shop.jsx
+++ client/src/pages/Shop.jsx
@@ -1,9 +1,19 @@
 import Header from "../components/Header";
 
+import "./styles/Shop.css";
+
 function Shop() {
   return(
-    <div>
-      <Header/>
+    <div className="shop-container">
+      <header className="shop-header">
+        <div className="shop-header-overlay">
+          <Header/>
+          <div className="shop-title-container">
+            <h1>SHOP</h1>
+            <p>Let Us Beautify your Home</p>
+          </div>
+        </div>
+      </header>
     </div>
   )
 }
