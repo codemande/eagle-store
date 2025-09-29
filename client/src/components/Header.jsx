0b37394a88e1550c2f15c36ddc700a7afbe0e366
codemande <1o0xday.2a10@gmail.com>
2025-09-29 17:31:51 +0100

    Input Cart Slide to header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,14 +4,16 @@ import { FaFacebook, FaInstagram, FaYoutube, FaShoppingCart } from "react-icons/
 import { FaXTwitter } from "react-icons/fa6";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { MdClose } from "react-icons/md";
+import CloseIcon from '@mui/icons-material/Close';
 import './styles/Header.css';
 
 function Header() {
-  const [menuOpen, setMenuOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false); 
+  const [cartOpen, setCartOpen] = useState(false);
 
   return (
-    <header>
-      <div className="header-desktop-container container">
+    <header className="header-desktop-header">
+      <div className="header-desktop-container container" >
 
         <Link to="/" className="header-logo-container container">
           <img className='logo' src="/plant-logo.png" alt="eagle store logo" />
@@ -43,7 +45,7 @@ function Header() {
           </span>
 
           <Link className="cart-icon-link">
-            <FaShoppingCart className="cart-icon"  />
+            <FaShoppingCart className="cart-icon" onClick={() => setCartOpen(true)} />
             <span className="cart-count bounce">0</span>
           </Link>
         </div>
@@ -59,29 +61,60 @@ function Header() {
       </div>
 
       {/* Mobile Nav Panel */}
-        {menuOpen && (
-          <div className="mobile-nav">
-            <Link to="/" onClick={() => setMenuOpen(false)}>
-              Home
-            </Link>
-            <Link to="/shop" onClick={() => setMenuOpen(false)}>
-              Shop
-            </Link>
-            <Link to="/about" onClick={() => setMenuOpen(false)}>
-              About
-            </Link>
-            <Link to="/contact" onClick={() => setMenuOpen(false)}>
-              Contact
-            </Link>
-            <Link onClick={() => setMenuOpen(false)}    className="cart-link-mobile">
-              <span style={{ position: "relative" }}>
-                <FaShoppingCart className="cart-icon"   />
-                <span  className="cart-count">0</span>
-              </span>
-              Cart
-            </Link>
-          </div>
-        )}
+      {menuOpen && (
+        <div className="mobile-nav">
+          <Link to="/" onClick={() => setMenuOpen(false)}>
+            Home
+          </Link>
+          <Link to="/shop" onClick={() => setMenuOpen(false)}>
+            Shop
+          </Link>
+          <Link to="/about" onClick={() => setMenuOpen(false)}>
+            About
+          </Link>
+          <Link to="/contact" onClick={() => setMenuOpen(false)}>
+            Contact
+          </Link>
+          <Link onClick={() => setMenuOpen(false)}    className="cart-link-mobile">
+            <span style={{ position: "relative" }}>
+              <FaShoppingCart className="cart-icon" onClick={() => setCartOpen(true)}  />
+              <span  className="cart-count">0</span>
+            </span>
+            Cart
+          </Link>
+          {cartOpen && <div className="header-cart-slide-container">
+            <div className="header-cart-slide-header">
+              <p className="header-cart-slide-title">Shopping Cart</p>
+              <p className="header-cart-slide-close" onClick={() => setCartOpen(false)}><CloseIcon/></p>
+            </div>  
+          
+            <div className="header-cart-slide-items">
+              <p className="header-cart-slide-content">No products in the cart.</p>
+            </div>
+            <div className="cart-slide-button-container">
+              <Link to="/shop" className="header-cart-slide-button">Continue Shopping</Link>
+            </div>
+          </div> }
+        </div>
+      )}
+
+      {/* Cart slide overlay */}
+      {cartOpen && <div className="header-cart-slide-overlay" onClick={() => setCartOpen(false)} ></div>}
+
+      {/* Cart side slide */}
+      {cartOpen && <div className="header-cart-slide-container">
+        <div className="header-cart-slide-header">
+          <p className="header-cart-slide-title">Shopping Cart</p>
+          <p className="header-cart-slide-close" onClick={() => setCartOpen(false)}><CloseIcon/></p>
+        </div>  
+       
+        <div className="header-cart-slide-items">
+          <p className="header-cart-slide-content">No products in the cart.</p>
+        </div>
+        <div className="cart-slide-button-container">
+          <Link to="/shop" className="header-cart-slide-button">Continue Shopping</Link>
+        </div>
+      </div>}
     </header>
   )
 }
