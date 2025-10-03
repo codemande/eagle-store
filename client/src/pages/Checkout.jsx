f787c8a8e42d2665cd9698730dd15dca8e3caf98
codemande <1o0xday.2a10@gmail.com>
2025-10-03 08:01:10 +0100

    Input Checkout, link to app and input header

---
+++ client/src/pages/Checkout.jsx
@@ -0,0 +1,15 @@
+
+import Header from "../components/Header";
+import "./styles/Cart.css"
+
+function Checkout() {
+  return(
+    <div>
+      <div className="cart-header-container">
+        <Header />
+      </div>
+    </div>
+  )
+}
+
+export default Checkout;
