b8fc6c17f04456378b9c1d4714a057e2344c92bc
codemande <1o0xday.2a10@gmail.com>
2025-10-06 15:48:28 +0100

    create context for cart, link to main.jsx

---
+++ client/src/context/CartContext.jsx
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+
+//create context
+const CartContext = createContext()
+
+export default CartContext;
---
+++ client/src/context/CartProvider.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import CartContext from "./CartContext";
+
+function CartProvider({ children }) {
+  const [cart, setCart] = useState([]);
+
+  //Add or increase product in Cart
+  function addToCart(product, qty = 1){
+    setCart((prev) => {
+
+      //if product already exist in cart
+      const existing = prev.find((item) => {
+        item.id === product.id
+      });
+      if (existing) {
+        return(
+          prev.map((item) => {
+            item.id === product.id ? { ...item, qty: item.qty + qty} : item
+          })
+        );
+      }
+
+      //if product do not exist
+      return [...prev, { ...product, qty }];
+    });
+  }
+
+  //Update quantity directly
+  function updateQty(id, qty) {
+    if(qty <= 0) {
+      return removeFromCart(id);
+    }
+
+    setCart((prev) => {
+      prev.map((item) => {
+        item.id === id ? { ...item, qty } : item
+      })
+    });
+  }
+
+  //Remove from cart
+  function removeFromCart(id) {
+    setCart((prev) => {
+      prev.filter((item) => {
+        item.id !== id
+      })
+    });
+  }
+
+  //Clear cart
+  function clearCart() {
+    setCart([]);
+  }
+
+  return(
+    <CartContext.Provider value={{ cart, addToCart, updateQty, removeFromCart, clearCart}}>
+      {children}
+    </CartContext.Provider>
+  );
+}
+
+export default CartProvider;
