238a49022bcd9559c75797e7b7fe7e7948cb8828
codemande <1o0xday.2a10@gmail.com>
2025-10-07 16:15:26 +0100

    Fix header cart slide, cart items reflect on cart slide, disactivate cart slide for mobile

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -12,10 +12,13 @@ function Header() {
   const [menuOpen, setMenuOpen] = useState(false); 
   const [cartOpen, setCartOpen] = useState(false);
 
-  const { cart } = useCart();
+  const { cart, removeFromCart } = useCart();
 
   //count total quantity instead of cart.length
   const cartCount = cart.reduce((sum, item) => sum + item.qty, 0);
+
+  // calculate total price
+  const total = cart.reduce((sum, item) => sum + item.price * item.qty, 0);
 
   return (
     <header className="header-desktop-header">
@@ -83,10 +86,7 @@ function Header() {
           <Link to="/contact" onClick={() => setMenuOpen(false)}>
             Contact
           </Link>
-          <Link onClick={() => {
-            setCartOpen(true);
-            setMenuOpen(false);
-          }} className="cart-link-mobile">
+          <Link to="/cart" onClick={() => {setMenuOpen(false);}} className="cart-link-mobile">
             <span style={{ position: "relative" }}>
               <FaShoppingCart className="cart-icon"/>
               <span key={cartCount} className="cart-count">
@@ -119,15 +119,55 @@ function Header() {
       {cartOpen && <div className="header-cart-slide-container">
         <div className="header-cart-slide-header">
           <p className="header-cart-slide-title">Shopping Cart</p>
-          <p className="header-cart-slide-close" onClick={() => setCartOpen(false)}><CloseIcon/></p>
+          <p className="header-cart-slide-close" onClick={() => setCartOpen(false)}>
+            <CloseIcon/>
+          </p>
         </div>  
        
         <div className="header-cart-slide-items">
-          <p className="header-cart-slide-content">No products in the cart.</p>
+          {cart.length === 0 ? 
+          <p className="header-cart-slide-content">No products in the cart.</p> :
+            cart.map((item) => {
+              return (
+                <div key={item.id}>
+                  <div className="cart-slide-items-container">
+                    <div className="cart-slide-image-container">
+                      <img src={`http://localhost:4100${item.image}`} className="cart-slide-item-image" alt={item.name} />
+                      <div className="cart-slide-name-container">
+                        <div className="cart-slide-item-name">{item.name}</div>
+                        <div className="cart-slide-item-price">{item.qty} X {`$${item.price}`}</div>
+                      </div>
+                    </div>
+                    <div className="cart-slide-item-remove">
+                      <CloseIcon style={{fontSize: "small", color: "#ddd"}} onClick={() => removeFromCart(item.id)}/>
+                    </div>
+                  </div>
+                  <hr style={{border: "1px solid #e9e9e9ff"}} />
+                </div>
+              )
+            })
+          }
         </div>
+
         <div className="cart-slide-button-container">
-          {/* testing cart link */}
-          <Link to="/cart" className="header-cart-slide-button">Continue Shopping</Link>
+          {cart.length === 0 ?
+            <Link to="/shop" className="header-cart-slide-button">
+              Continue Shopping
+            </Link>
+            :
+            <div>
+              <div className="header-cart-slide-subtotal-wrap">
+                <p>Subtotal:</p>
+                <p>${total}</p>
+              </div>
+              <Link to="/cart" className="header-cart-slide-button">
+                View Cart
+              </Link>
+              <Link to="/checkout" className="header-cart-slide-button">
+                Checkout
+              </Link>
+            </div>
+          }
         </div>
       </div>}
     </header>
