fa8cf84f460e5f39ecb0822013c12533dfd4ef9b
codemande <1o0xday.2a10@gmail.com>
2025-10-24 04:27:45 +0100

    Create login successfull popup on home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect, useContext } from "react";
 import HomeFixtures from "../components/home/HomeFixtures.jsx";
 import Header from "../components/Header";
 import HomeDivider1 from "../components/home/HomeDivider1.jsx";
@@ -9,11 +10,48 @@ import HomePopularProduct from "../components/home/HomePopularProduct.jsx";
 import HomeTestimonies from "../components/home/HomeTestimonies.jsx";
 import HomeDivider2 from "../components/home/HomeDivider2.jsx";
 import Footer from "../components/Footer.jsx";
+import AuthContext from "../context/AuthContext.jsx";
+import { MdClose } from "react-icons/md";
 import "./styles/Home.css";
 
 function Home() {
+  const [showPopUp, setShowPopUp] = useState(false);//logout notification
+  const [timer, setTimer] = useState(null);// clear logout notification
+  const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+
+    if (!user) return;// run only when user exists
+
+    // Clear any previous timer
+    if (timer) clearTimeout(timer);
+
+    setShowPopUp(true);
+
+
+    const newTimer = setTimeout(() => {
+      setShowPopUp(false);
+    }, 5000);
+
+    setTimer(newTimer)
+
+    // Cleanup on unmount or user change
+    return () => clearTimeout(newTimer);
+    
+  }, [user]);
+
+  function removePopUp() {
+    if(timer) clearTimeout(timer);
+    setShowPopUp(false);
+  }
   return(
     <div className="home-page-container">
+
+      {showPopUp && 
+        <div className="home-page-popup">
+          Login Successful! <MdClose onClick={removePopUp} className="addtocart-notification-close" />
+        </div>
+      }
 
       <div className="hero-home-page-container">
         <div className="hero-page-overlay">
