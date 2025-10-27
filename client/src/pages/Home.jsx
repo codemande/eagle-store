8c79440a01e97177544fbbc6c4ca4e67d6da9516
codemande <1o0xday.2a10@gmail.com>
2025-10-27 13:38:41 +0100

    remove login sucessful pop up in home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -11,47 +11,12 @@ import HomeTestimonies from "../components/home/HomeTestimonies.jsx";
 import HomeDivider2 from "../components/home/HomeDivider2.jsx";
 import Footer from "../components/Footer.jsx";
 import AuthContext from "../context/AuthContext.jsx";
-import { MdClose } from "react-icons/md";
 import "./styles/Home.css";
 
 function Home() {
-  const [showPopUp, setShowPopUp] = useState(false);//logout notification
-  const [timer, setTimer] = useState(null);// clear logout notification
-  const { user } = useContext(AuthContext);
 
-  useEffect(() => {
-
-    if (!user) return;// run only when user exists
-
-    // Clear any previous timer
-    if (timer) clearTimeout(timer);
-
-    setShowPopUp(true);
-
-
-    const newTimer = setTimeout(() => {
-      setShowPopUp(false);
-    }, 5000);
-
-    setTimer(newTimer)
-
-    // Cleanup on unmount or user change
-    return () => clearTimeout(newTimer);
-    
-  }, []);
-
-  function removePopUp() {
-    if(timer) clearTimeout(timer);
-    setShowPopUp(false);
-  }
   return(
     <div className="home-page-container">
-
-      {showPopUp && 
-        <div className="home-page-popup">
-          Login Successful! <MdClose onClick={removePopUp} className="addtocart-notification-close" />
-        </div>
-      }
 
       <div className="hero-home-page-container">
         <div className="hero-page-overlay">
