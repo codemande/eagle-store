28cd1dbe14bd2158249467d549e4a7c905219e35
codemande <1o0xday.2a10@gmail.com>
2026-03-10 00:46:34 +0100

    Initial app.js setup

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,18 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
+// Basic configurations
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// Cors configuration
+app.use(cors({
+  origin: process.env.CLIENT_URL || "http://localhost:5173",
+  credentials: true,
+  methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
 
 export default app;
