46ebe828ad734f9e508c9fa7f2a0fbfac5339402
codemande <1o0xday.2a10@gmail.com>
2026-03-10 09:37:46 +0100

    Create Constants

---
+++ server/src/utils/constants.js
@@ -0,0 +1,14 @@
+export const UserRolesEnum = {
+  ADMIN: "admin",
+  USER: "user"
+}
+
+export const AvailableUserRole = Object.values(UserRolesEnum);
+
+export const TaskStatusEnum = {
+  TODO: "todo",
+  IN_PROGRESS: "in_progress",
+  DONE: "done",
+}
+
+export const AvailableTaskStatues = Object.values(TaskStatusEnum);
