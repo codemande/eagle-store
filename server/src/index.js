4205e769f186a4625642e1e8bd8ff3fdbc7472fa
codemande <1o0xday.2a10@gmail.com>
2026-03-11 11:34:31 +0100

    Connect Mongoose to MongoDB. Install MongoDB

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,20 @@
+import dotenv from "dotenv";
 import app from "./app.js";
+import connectDB from "./db/index.js";
 
 dotenv.config({
-  path: "./.env",
+  path: "../.env",
 });
 
 const PORT = process.env.PORT || 4100;
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running at http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error", err);
+    process.exit(1);
+  })
