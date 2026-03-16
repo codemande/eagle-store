2305dd30bacdcde49e221e334b9f859a158599fc
codemande <1o0xday.2a10@gmail.com>
2026-03-16 15:09:33 +0100

    feat: Create getCurrentUser controller

--- server/src/controllers/auth.controllers.js
+++ server/src/controllers/auth.controllers.js
@@ -145,4 +145,18 @@ const logoutUser = asyncHandler(async (req, res) => {
     );
 });
 
-export { registerUser, login, logoutUser };
+const getCurrentUser = asyncHandler(async (req, res) => {
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        req.user,
+        "Current user fetched successfully"
+      )
+    )
+})
+
+// const getCurrentUser = asyncHandler(async (req, res) => {})
+
+export { registerUser, login, logoutUser, getCurrentUser };
