993be34f704d17445d51328dd494f9aabf5db357
codemande <1o0xday.2a10@gmail.com>
2026-03-20 00:09:01 +0100

    Change api response map for popular products on home page

--- client/src/components/home/HomePopularProduct.jsx
+++ client/src/components/home/HomePopularProduct.jsx
@@ -8,7 +8,7 @@ import useCart from "../../context/useCart";
 import "./styles/DisplayProduct.css";
 
 function HomePopularProduct() {
-  const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:4100";
+  const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:4000";
 
   const [params] = useSearchParams();
   const q = params.get("query") || "";
@@ -48,8 +48,8 @@ function HomePopularProduct() {
     setError(null);
 
     axios
-      .get(`${API_BASE_URL}/api/products`, { params: { query: q } })
-      .then((res) => setItems(res.data))
+      .get(`${API_BASE_URL}/api/v1/products`, { params: { query: q } })
+      .then((res) => setItems(res.data.data))
       .catch((err) => setError(err.message))
       .finally(() => setLoading(false));
   }, [q]);
@@ -74,7 +74,7 @@ function HomePopularProduct() {
                 <Link to={`/product/${p.slug}`} className="displayProduct-item" key={p.id}>
                   <div className="displayProduct-image-container">
 
-                    <img className="displayProduct-image" src={ p.image.startsWith("http") ? p.image : `${import.meta.env.VITE_API_URL || "http://localhost:4100"}${p.image}`} alt={p.name} />
+                    <img className="displayProduct-image" src={ p.image } alt={p.name} />
 
                     <div className="displayProduct-cart-container">
                       <div className="displayProduct-cart-tooltip">
